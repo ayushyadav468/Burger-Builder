6e1eb634ed8fc309ee97d6a73015da69d2488cc9
ayushyadav468 <ayushyadav468@gmail.com>
2020-12-03 14:47:36 +0530

    build controls added

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './BurgerIngredient.module.css';
 
 const burgerIngredient = (props) => {
@@ -33,6 +34,9 @@ const burgerIngredient = (props) => {
   }
 
   return ingredient;
+};
+burgerIngredient.propTypes = {
+  type: PropTypes.string.isRequired,
 };
 
 export default burgerIngredient;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
-
 import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      cheese: 2,
+      meat: 0,
+      bacon: 0,
+    },
+  };
   render() {
     return (
       <Aux>
-        <div>Burger</div>
-        <div>Burger Controls</div>
+        <Burger ingredients={this.state.ingredients} />
+        <BuildControls />
       </Aux>
     );
   }
